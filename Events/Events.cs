02937a927173c510d2f86b5599455aa7dec3d385
Luiz Wendt <luizaugustow_1@msn.com>
2018-02-23 02:59:36 -0300

    Removed signals and come back Event

---
+++ Events/Events.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic;
+
+/// <summary>
+/// Event system object
+/// created by Ben D'Angelo. Available on: https://gist.github.com/bendangelo/093edb33c2e844c5c73a
+/// <example>
+/// <code>
+///  public class ExampleEvent : GameEvent
+///    {
+///
+///    }
+///
+///    public class ExampleEventWithProperties : GameEvent
+///    {
+///        public float _myFloat;
+///
+///        public ExampleEventWithProperties(float f)
+///        {
+///            _myFloat = f;	
+///        }
+///    }
+/// </code>
+/// </example>
+/// </summary>
+    public class GameEvent    {    }
+
