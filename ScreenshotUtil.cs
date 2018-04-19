d635f41fd3e5b264b75ef02967bea800b93fc209
Luiz Wendt <luiz.wendt@owlbytes.com>
2018-04-19 16:56:29 -0300

    added screenshot utils

---
+++ ScreenshotUtil.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenshotUtil : Singleton<ScreenshotUtil>
+{
+    [SerializeField]
+    private int _screenshotNumber;
+    public KeyCode keyCode = KeyCode.P;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(keyCode))
+        {
+            Capture();
+        }
+    }
+
+    [EasyButtons.Button()]
+    void Capture()
+    {
+        string path = Application.persistentDataPath + "/screenshot_" + _screenshotNumber + ".png";
+        ScreenCapture.CaptureScreenshot(path);
+        Debug.Log("screenshot was taken and saved to " + path);
+        _screenshotNumber++;
+
+    }
+}
