367fc8dfe25e3269a236875f4bcf3acafaad94cd
Luiz Augusto Wendt <luiz.wendt@outlook.com>
2019-01-15 07:28:24 -0200

    added scene reference

---
+++ Assets/Plugin/Snippets/Scene/SceneReference.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+
+namespace LUT.Snippets
+{
+    [Serializable]
+    public class SceneReference
+    {
+#pragma warning disable 0649
+#if UNITY_EDITOR
+        public UnityEditor.SceneAsset scene;
+#endif
+
+        [InspectorReadOnly]
+        [SerializeField]
+        private string sceneName;
+#pragma warning restore 0649
+
+        public string SceneToLoadName
+        {
+            get
+            {
+                return sceneName;
+            }
+            set
+            {
+                sceneName = value;
+            }
+        }
+    }
+}
