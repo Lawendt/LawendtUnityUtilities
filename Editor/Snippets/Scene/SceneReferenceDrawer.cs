a8f8d26f1875008d496d17014368cb256f7cb25c
Luiz Augusto Wendt <luiz.wendt@outlook.com>
2019-01-15 07:28:24 -0200

    added scene reference

---
+++ Editor/Snippets/Scene/SceneReferenceDrawer.cs
@@ -0,0 +1,43 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace LUT.Snippets
+{
+    [CustomPropertyDrawer(typeof(SceneReference))]
+    public class SceneReferenceDrawer : PropertyDrawer
+    {
+
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            return EditorGUIUtility.singleLineHeight* 2;
+        }
+
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            EditorGUI.BeginProperty(position, label, property);
+            EditorGUI.BeginChangeCheck();
+
+            SerializedProperty sceneObject = property.FindPropertyRelative("scene");
+            SerializedProperty sceneName = property.FindPropertyRelative("sceneName");
+            EditorGUI.PropertyField(position, sceneObject);
+            position.y += EditorGUIUtility.singleLineHeight;
+
+            EditorGUI.PropertyField(position, sceneName);
+            if (EditorGUI.EndChangeCheck())
+            {
+                property.serializedObject.ApplyModifiedProperties();
+                SceneAsset scene = sceneObject.objectReferenceValue as SceneAsset;
+                if (scene)
+                {
+                    sceneName.stringValue = scene.name;
+                }
+                else
+                {
+                    sceneName.stringValue = "null";
+                }
+            }
+            EditorGUI.EndProperty();
+        }
+
+    }
+}
