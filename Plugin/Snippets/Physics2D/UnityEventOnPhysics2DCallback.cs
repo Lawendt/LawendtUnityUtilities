83de246eed42dde3f9b0024a934e4b8179649805
Luiz Augusto Wendt <luiz.wendt@outlook.com>
2019-02-01 01:27:29 -0200

    Added OnCallback scripts that give easy acces to callbacks from mono

---
+++ Plugin/Snippets/Physics2D/UnityEventOnPhysics2DCallback.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine.Events;
+
+namespace LUT
+{
+
+	public class UnityEventOnPhysics2DCallback : OnPhysics2DCallback
+	{
+		public UnityEvent execute;
+		public override void Execute()
+		{
+			execute.Invoke();
+		}
+	}
+}
