a757104fb4aca45997213d3d958fcbbd02a7e0ad
Luiz Wendt <luiz.wendt@owlbytes.com>
2019-04-17 18:54:26 -0300

    fixed bug on serialization of event bool

--- Runtime/ScriptableObjects/Events/Primitives/EventBool.cs
+++ Runtime/ScriptableObjects/Events/Primitives/EventBool.cs
@@ -2,10 +2,5 @@ namespace LUT.Events.Primitives
 {
 	public sealed class EventBool : EventObject<bool>
 	{
-#if UNITY_EDITOR
-#pragma warning disable 0414
-		[UnityEngine.SerializeField]
-		private bool valueToInvokeWith = false;
-#endif
 	}
 }
